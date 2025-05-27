8749856719b1a8c5401ed70e93d6ade3fb8c92c1
Moimaritim <moimaritim@gmail.com>
2025-05-27 15:26:28 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,7 +2,6 @@
 """
 Route module for the API
 """
-from api.v1.auth.auth import Auth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -16,7 +15,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = os.getenv('AUTH_TYPE')
 
-if auth_type == 'auth':
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class for handling Basic Authentication
+    """
+    pass
