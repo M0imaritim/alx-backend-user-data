d40860a779beeff558b3f5a591684fa2a46d79f1
Moimaritim <moimaritim@gmail.com>
2025-06-06 11:43:44 +0300

    pycodestyle fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,22 +34,23 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
-def login() -> str:
-    """Log in user and create session."""
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    POST /sessions.
+
+    Creates a new session if login is valid.
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not auth.valid_login(email, password):
+    if auth.valid_login(email, password):
+        session_id = auth.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
         abort(401)
-
-    session_id = auth.create_session(email)
-    if not session_id:
-        abort(401)
-
-    response = make_response(jsonify({"email": email, "message": "logged in"}))
-    response.set_cookie("session_id", session_id)
-    return response
 
 
 if __name__ == "__main__":
